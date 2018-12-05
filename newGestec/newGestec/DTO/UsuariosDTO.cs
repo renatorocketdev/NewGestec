21d90cc1598f128626b24cc1baa52678336d88c4
Caio Sales <caiofernandosales@outlook.com>
2018-12-04 23:59:16 -0300

    DTO 1.0
    DTO Usuários
    DTO Empresa
    DTO Profissionais

---
+++ newGestec/newGestec/DTO/UsuariosDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace newGestec.DTO
+{
+    class UsuariosDTO
+    {
+        public class Usuarios
+        {
+            private int id;
+            public int Id { get => id; set => id = value; }      
+            
+            private string email;
+            public string Email { get => email; set => email = value; }  
+            
+            private string senha;
+            public string Senha { get => senha; set => senha = value; }   
+            
+            private string cpf;
+            public string Cpf { get => cpf; set => cpf = value; }
+
+            private string nascimento;
+            public string Nascimento { get => nascimento; set => nascimento = value; }
+
+            private string permissao;
+            public string Permissao { get => permissao; set => permissao = value; }
+        }
+    }
+}
